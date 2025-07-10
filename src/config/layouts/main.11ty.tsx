43a30a6dac4f012f7ec35041f57df642ddb34134
j-mes <dev@j-m.es>
2025-07-10 07:06:02 +0100

    chore: update main tsx template

---
+++ src/config/layouts/main.11ty.tsx
@@ -0,0 +1,15 @@
+import { ViewProps } from '../../../eleventy.config.ts';
+
+export function siteLayout({ content, title }: ViewProps): JSX.Element {
+	return (
+		<html lang="en-GB">
+			<head>
+				<title>{title ?? 'No title'}</title>
+				<link rel="stylesheet" href="/style.css" />
+			</head>
+			<body>{content}</body>
+		</html>
+	);
+}
+
+export const render = siteLayout;
