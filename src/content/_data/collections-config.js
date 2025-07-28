6d8b2dd43cbfcd9e12ab1875cc4b864d0a253928
j-mes <dev@j-m.es>
2025-07-28 08:30:00 +0000

    refactor: add src/content/_data/

---
+++ src/content/_data/collections-config.js
@@ -0,0 +1,6 @@
+// This file exposes collectionsConfig as Eleventy global data for use in templates.
+// It is generated from src/helpers/collections-config.ts for type safety.
+
+import collectionsConfig from '../../helpers/collections-config.js';
+
+export default collectionsConfig;
