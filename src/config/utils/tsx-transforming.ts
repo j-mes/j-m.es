41ff1ab4db94a4d6a6cde4e7d40767cfc737d307
j-mes <dev@j-m.es>
2025-07-28 08:30:00 +0000

    refactor: remove src/config/utils/tsx-transforming.ts

--- src/config/utils/tsx-transforming.ts
+++
@@ -1,6 +0,0 @@
-import { jsxToString } from 'jsx-async-runtime';
-
-export async function tsxTransform(content: string): Promise<string> {
-	const result = await jsxToString(content);
-	return `<!doctype html>\n${result}`;
-}
